faea67c076e8b52ef5554604156e9b422860a043
MaryAznivG <marygalstyan10@gmail.com>
2023-10-27 20:45:17 -0700

    added API documentation comment

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -13,6 +13,7 @@ export default function Dictionary() {
     event.preventDefault();
     /* so it won't reload the page and add event to the property too^ */
 
+    //documentation:http://dictionaryapi.dev/ (api link)
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
