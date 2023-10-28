218fad22a69eb5a377c53d47e9c54cee0568df47
MaryAznivG <marygalstyan10@gmail.com>
2023-10-27 20:15:10 -0700

    installed axios and fixed errors

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -5,21 +5,16 @@ export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   function search(event) {
     event.preventDefault();
-    {
-      /* so it won't reload the page and add event to the property too^ */
-    }
+    /* so it won't reload the page and add event to the property too^ */
     alert(`Searching for ${keyword} definition`);
   }
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
-    {
-      /* whenever we want to change the state we use setkeyword  */
-    }
+    /* whenever we want to change the state we use setkeyword  */
   }
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
-        {" "}
         {/* add a eventlister on the form "onSubmit" whenever someone submits the form we can call a function "search" then we need to define this function in the component ^  */}
         <input type="search" onChange={handleKeywordChange} />{" "}
         {/* To know the value what it's searching we must add usestate^ So whenever a person types or changes a word it's going to be the handleKeywordChange function*/}
