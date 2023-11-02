4ad34e980140f7f5ece544261cdebef54e8d086e
MaryAznivG <marygalstyan10@gmail.com>
2023-11-01 22:43:52 -0700

    adding logo & CSS. added a default word when loads

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import logo from "./dictionary-logo.png";
 import Dictionary from "./Dictionary";
 import "./App.css";
 
@@ -6,9 +7,12 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">Dictionary</header>
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+          Dictionary
+        </header>
         <main>
-          <Dictionary />
+          <Dictionary defaultKeyword="sunset" />
         </main>
         <footer className="App-Footer">
           <small> Coded by Azniv Mary Galstyan </small>
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,37 +3,57 @@ import axios from "axios";
 import Results from "./Results";
 import "./Dictionary.css";
 
-export default function Dictionary() {
-  let [keyword, setKeyword] = useState("");
+export default function Dictionary(props) {
+  let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
-  //everytime the state changes it's going render again with a new data. Basically, we want the definition to change everytime the result does so therefore we create a state called results.
+  let [loaded, setLoaded] = useState(false);
+  //everytime the state changes it's going render again with a new data. Basically, we want the definition to change everytime the result does so therefore we create a state called results. have a default word when the page loads
 
   function handleResponse(response) {
-    console.log(response.data[0]);
     setResults(response.data[0]);
   }
 
-  function search(event) {
+  function handleSubmit(event) {
     event.preventDefault();
+    search();
     /* so it won't reload the page and add event to the property too^ */
+  }
 
+  function search() {
     //documentation:http://dictionaryapi.dev/ (api link)
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+    /* whenever we want to change the state we use setkeyword  */
   }
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
     /* whenever we want to change the state we use setkeyword  */
   }
-  return (
-    <div className="Dictionary">
-      <form onSubmit={search}>
-        {/* add a eventlister on the form "onSubmit" whenever someone submits the form we can call a function "search" then we need to define this function in the component ^  */}
-        <input type="search" onChange={handleKeywordChange} />{" "}
-        {/* To know the value what it's searching we must add usestate^ So whenever a person types or changes a word it's going to be the handleKeywordChange function*/}
-      </form>
-      <Results results={results} />
-    </div>
-  );
+
+  function load() {
+    setLoaded(true);
+    search();
+  }
+
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <section>
+          <form onSubmit={handleSubmit}>
+            <input type="search" onChange={handleKeywordChange} />
+          </form>
+          <div className="hint">
+            suggested words: sunset, wine, yoga, love, forest...
+          </div>
+        </section>
+        <Results results={results} />
+      </div>
+    );
+  } else {
+    load();
+    return "Loading";
+  }
+  /*add a eventlister on the form "onSubmit" whenever someone submits the form we can call a function "search" then we need to define this function in the component ^  */
+  /* To know the value what it's searching we must add usestate^ So whenever a person types or changes a word it's going to be the handleKeywordChange function*/
 }
