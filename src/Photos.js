d96eab620ba15f1c5be3ece506f4d60af666268e
MaryAznivG <marygalstyan10@gmail.com>
2023-11-05 18:16:18 -0800

    revised

--- src/Photos.js
+++ src/Photos.js
@@ -7,12 +7,17 @@ export default function Photos(props) {
       <section className="Photos">
         <div className="row">
           {props.photos.map(function (photo, index) {
-            return;
-            <div className="col-4" key={index}>
-              <a href={photo.src.original} target="_blank" rel="noreferrer">
-                <img src={photo.src.landscape} className="img-fluid" />
-              </a>
-            </div>;
+            return (
+              <div className="col-4" key={index}>
+                <a href={photo.src.original} target="_blank" rel="noreferrer">
+                  <img
+                    src={photo.src.landscape}
+                    className="img-fluid"
+                    alt={photo.src.photographer}
+                  />
+                </a>
+              </div>
+            );
           })}
         </div>
       </section>
